f02f8d4b3e6a34575b7a83136abba3b8aa69224e
Yegor Zorin <nezort11@gmail.com>
2023-09-25 14:13:26 +0300

    fix: Change custom puppeteer chromium exec path

--- image-translate/src/translateimage.ts
+++ image-translate/src/translateimage.ts
@@ -24,6 +24,7 @@ const initTranslateImage = async () => {
       ...(IS_PRODUCTION
         ? {
             headless: true,
+            executablePath: '/usr/bin/chromium',
           }
         : {
             headless: false,
