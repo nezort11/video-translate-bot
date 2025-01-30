aa20dc7ea415eced4b3007ce25bb6dffe8a5f465
nezort11 <nezort11@gmail.com>
2025-01-30 18:02:39 +0300

    feat: Add ytdl cookies guide

--- src/services/ytdl.ts
+++ src/services/ytdl.ts
@@ -2,6 +2,9 @@ import fs from "fs";
 import path from "path";
 import ytdl from "@distube/ytdl-core";
 
+// 1. Install https://chromewebstore.google.com/detail/cclelndahbckbenkjhflpdbgdldlbecc
+// 2. Go to https://youtub.com (feed index page)
+// 3. Click on extension, select "JSON" and "copy" into cookies.json file
 const COOKIES_FILENAME = "cookies.json";
 const COOKIES_FILE_PATH = path.join(
   __dirname,
