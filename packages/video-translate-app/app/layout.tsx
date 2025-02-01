a6fc59c41720e15087948f5cf9003c4843f5b1a2
nezort11 <nezort11@gmail.com>
2025-02-01 14:35:45 +0300

    feat: Add mini app speed insights

--- packages/video-translate-app/app/layout.tsx
+++ packages/video-translate-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -37,6 +38,7 @@ export default function RootLayout({
         <Suspense>{children}</Suspense>
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
