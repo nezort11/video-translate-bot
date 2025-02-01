d8ef3ae4e08cdeefe674fcf07316a3c593898429
nezort11 <nezort11@gmail.com>
2025-02-01 14:58:26 +0300

    feat: Add sentry error tracking and monitoring

---
+++ packages/video-translate-app/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c4b89669b2444352b3d04ca89df9d94e@o1075549.ingest.us.sentry.io/4505279528108032",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
