cda7019087d5bce7d1cf9fc7fb7559e574b1e363
nezort11 <nezort11@gmail.com>
2025-02-01 14:08:36 +0300

    feat: Add mini app vercel web analytics

--- packages/video-translate-app/app/layout.tsx
+++ packages/video-translate-app/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <Suspense>{children}</Suspense>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
