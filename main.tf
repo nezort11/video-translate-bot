3be1814bbaa4211cfa238471a25f800c0dcc173e
nezort11 <nezort11@gmail.com>
2025-02-17 15:13:49 +0300

    feat: Add yam push message api gateway for webhook

--- main.tf
+++ main.tf
@@ -36,6 +36,30 @@ resource "yandex_function_trigger" "video-translate-bot-queue-trigger" {
   container {
     id = var.container_id
     service_account_id = var.service_account_id
-    path = "/queue"
+    path = "/queue/callback"
   }
+}
+
+resource "yandex_api_gateway" "video-translate-bot-queue-gateway" {
+  name        = "video-translate-bot-queue-gateway"
+  description = "API Gateway that receives Telegram updates and pushes them to Yandex Message Queue."
+
+  spec = <<EOF
+openapi: 3.0.0
+info:
+  title: "Video Translate Bot Queue API Gateway"
+  version: "1.0.0"
+paths:
+  /callback:
+    post:
+      x-yc-apigateway-integration:
+        type: cloud_ymq
+        action: SendMessage
+        queue_url: "${yandex_message_queue.video-translate-bot-queue.id}"
+        folder_id: "${var.yc_folder_id}"
+        service_account_id: "${var.service_account_id}"
+      responses:
+        "200":
+          description: "OK"
+EOF
 }
